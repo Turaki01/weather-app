041de62075d416c4efcfe4a1f742178c0bf53021
Turaki01 <ezekiel_Zakari@yahoo.com>
2020-03-07 09:03:45 +0100

    added destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request');
 const forecast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/36bd7836e2dd9a63504b4c701426c52a/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?units=si';
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather service', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            const currentlyData = response.body.currently;
-            const dailyData = response.body.daily;
+            const currentlyData = body.currently;
+            const dailyData = body.daily;
 
             callback(undefined, `${dailyData.data[0].summary} It is currently ${currentlyData.temperature} degrees out. There is a ${currentlyData.precipProbability}% chance of rain.`)
         }
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,14 +3,14 @@ const request = require('request');
 const geoCode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidHVyYWtpIiwiYSI6ImNrN2V4Z2J3aDExenUzbnBha3M0am5qeTgifQ.AkJjOd3xZuYar5Ne5cmQzw&limit=1';
 
-    request({url: url, json: true}, (error, response) => {
-        if(error) {
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            const cordinates = response.body.features[0].center;
-            const placeName = response.body.features[0].place_name;
+            const cordinates = body.features[0].center;
+            const placeName = body.features[0].place_name;
             callback(undefined, {
                 latitude: cordinates[1],
                 longitude: cordinates[0],
